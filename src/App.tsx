4b809e033225f343467d6e09f0a107d5d3d6fbf9
DanaGrin1105 <67092775+DanaGrin1105@users.noreply.github.com>
2021-11-16 10:37:33 +0200

    Display TOC and first result

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,40 @@ import "./styles.css";
 
 const getMovies = () => {
   return fetch("https://swapi.dev/api/films/").then((results) => {
-     return results.json()
+    return results.json();
   });
 };
 
 export default function App() {
   const [data, setData] = useState(null);
+  const [chosenMovie, setChosenMovie] = useState(0);
 
   useEffect(() => {
-    getMovies().then((res) => setData(res.results));
+    getMovies().then((res) => {
+      setData(res.results);
+      setChosenMovie(res.results[0]);
+    });
   }, []);
 
+  if (!data) {
+    return <main className="App">Loading...</main>;
+  }
+
   console.log(data);
+  console.log({ chosenMovie });
   return (
     <main className="App">
       <section className="toc">
-        sidebar / TOC
+        <h1>TOC</h1>
+        <ul>
+          {data?.map((movie) => (
+            <li>{movie.title}</li>
+          ))}
+        </ul>
       </section>
       <section className="movie-details">
-        movie details
+        <h1>{chosenMovie.title}</h1>
+        <p>{chosenMovie.opening_crawl}</p>
       </section>
     </main>
   );
