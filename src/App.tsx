345dc78d48906097d19daddd8f443a4a00db02cb
DanaGrin1105 <67092775+DanaGrin1105@users.noreply.github.com>
2021-11-16 10:28:15 +0200

    seting up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
 
-const getMovies = async () => {
-  const res = await fetch("https://swapi.dev/api/films/");
-  const results = await res?.json();
-  return results;
+const getMovies = () => {
+  return fetch("https://swapi.dev/api/films/").then((results) => {
+     return results.json()
+  });
 };
 
 export default function App() {
@@ -16,9 +16,13 @@ export default function App() {
 
   console.log(data);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <main className="App">
+      <section className="toc">
+        sidebar / TOC
+      </section>
+      <section className="movie-details">
+        movie details
+      </section>
+    </main>
   );
 }
